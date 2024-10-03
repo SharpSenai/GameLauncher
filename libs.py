f4abd074d6f6bf7778f26ee457786660b1efc5ce
NightlyOneV <joserobertomiranda.contato@gmail.com>
2024-10-03 10:38:03 -0300

    libs Renamed

--- libs.py
+++ libs.py
@@ -1,7 +1,8 @@
-import time 
-import numpy 
-import PySide6
-import customtkinter
-import seaborn
-import requests 
-import sqlite3
+import time as Lib_Time
+import numpy as Lib_Numpy
+import PySide6 as Lib_PySide
+import customtkinter as Lib_CTK
+import seaborn as Lib_Seaborn 
+import requests as Lib_Requests
+import sqlite3 as Lib_SQLite3
+import math as Lib_Math
